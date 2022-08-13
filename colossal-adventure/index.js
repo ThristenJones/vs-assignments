198327c11e161acee7b523751257923c10cb5918
Thristen Jones <Thristenjones@yahoo.com>
2022-08-12 17:37:17 -0700

    initial commit plus installed readline-sync

---
+++ colossal-adventure/index.js
@@ -0,0 +1,11 @@
+const readline = require('readline-sync');
+
+console.log('Hello there, welcome to colossal adventures!')
+
+const name = readline.question("May we please get your name before we continue? ");
+
+
+
+console.log(`Thank you ${name}. Now please press "w" to start walking` );
+
+if()
