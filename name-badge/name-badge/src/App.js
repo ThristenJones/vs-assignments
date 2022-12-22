79c2bbe7c8893f9a0660be1f89558a217435aa3d
Thristen Jones <Thristenjones@yahoo.com>
2022-12-22 12:14:56 -0700

    created a name badge website to collect basic user info from users

---
+++ name-badge/name-badge/src/App.js
@@ -0,0 +1,155 @@
+import React from 'react';
+import './App.css'
+
+export default function App(){
+
+    const [formData, setFormData] = React.useState({
+        firstName: "", lastName: "", email: "", birthPlace: "", phoneNumber: "", favoriteFoods: "", comments: ""
+    }) 
+
+    const [badgeFormData, setBadgeFormData] = React.useState([])
+
+
+    function handleChange(event){
+        const {name, value} = event.target
+        setFormData(prevValue => ({
+            ...prevValue,
+            [name]: value
+        }))
+    }
+
+    const formArray = badgeFormData.map(data => {
+        
+
+        return( 
+        <div>
+        
+            <p className="title"><b>BADGE:</b></p>
+            <div className="badge">
+                <p><b>Name:</b> {formData.firstName} {formData.lastName}</p> 
+
+                <p><b>Place Of Birth:</b> {formData.birthPlace}</p>
+                
+                <p><b>Email:</b> {formData.email}</p>
+
+                <p><b>Phone:</b> {formData.phoneNumber}</p>
+
+                <p><b>Favorite Food:</b> {formData.favoriteFoods}</p>
+
+                <textarea className="comments">{formData.comments}</textarea>
+            </div>
+        </div>
+        )
+    })
+
+    function handleSubmit(event){
+        event.preventDefault()
+        
+        if(formData.firstName === ''){
+            alert('Please fill in all inputs: FIRST NAME')
+            return
+        }
+        if(formData.lastName === ''){
+            alert('Please fill in all inputs: LAST NAME')
+            return
+        }
+        if(formData.email === ''){
+            alert('Please fill in all inputs: EMAIL')
+            return
+        }
+        if(formData.birthPlace === ''){
+            alert('Please fill in all inputs: BIRTH PLACE')
+            return
+        }
+        if(formData.phoneNumber === ''){
+            alert('Please fill in all inputs: PHONE NUMBER')
+            return
+        }
+        if(formData.favoriteFoods === ''){
+            alert('Please fill in all inputs: FAVORITE FOOD')
+            return
+        }
+        setBadgeFormData(prevValue => [...prevValue, formData]);
+
+    }
+
+    return(
+        <form onSubmit={handleSubmit}>
+            <h1 className='badgeTitle'> Name Badge Entry</h1>
+            <div className="container">
+                <input
+                    className="item1"
+                    type="text"
+                    name="firstName"
+                    placeholder='First Name'
+                    value={formData.firstName}
+                    onChange={handleChange}
+                    minLength='3'
+                ></input>
+
+                <input
+                    className="item2"
+                    type="text"
+                    name="lastName"
+                    placeholder='Last Name'
+                    value={formData.lastName}
+                    onChange={handleChange}
+                    minLength='3'
+                ></input>
+
+                <input
+                    className="item3"
+                    type="text"
+                    name="email"
+                    placeholder='Email'
+                    value={formData.email}
+                    onChange={handleChange}
+                    minLength='3'
+                ></input>
+
+                <input
+                    className="item4"
+                    type="text"
+                    name="birthPlace"
+                    placeholder='Place Of Birth'
+                    value={formData.birthPlace}
+                    onChange={handleChange}
+                    minLength='3'
+                ></input>
+
+                <input
+                    className="item5"
+                    type="text"
+                    name="phoneNumber"
+                    placeholder='Phone Number'
+                    value={formData.phoneNumber}
+                    onChange={handleChange}
+                    minLength='3'
+                ></input>
+
+                <input
+                    className="item6"
+                    type="text"
+                    name="favoriteFoods"
+                    placeholder='Favorite Food'
+                    value={formData.favoriteFoods}
+                    onChange={handleChange}
+                    minLength='3'
+                ></input>
+
+                <textarea 
+                    className='textarea'
+                    type="text"
+                    name="comments"
+                    placeholder='Tell us about yourself?'
+                    value={formData.comments}
+                    onChange={handleChange}
+                    minLength='3'
+                />
+                <button className='button'>Submit</button>
+            </div>
+            
+           {formArray}
+        </form>
+    )
+}
